d243bead7bb9d490b9742b19c9d91a2f98ef8311
razvan-turiatca <codingwithrazvan@gmail.com>
2022-11-09 23:00:09 +0200

    upload completed tour project

---
+++ react simple projects/tours-project/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
+function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [tours, setTours] = useState([])
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id != id)
+    setTours(newTours)
+  }
+  const fetchTours = async () => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setTours(data)
+
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
+}
+
+export default App
