3545551c8e52bfc2b93b78ed0d8850495b9eba8f
razvan-turiatca <codingwithrazvan@gmail.com>
2023-01-03 19:47:19 +0200

    handle the card size

--- flashcard-app/src/App.js
+++ flashcard-app/src/App.js
@@ -34,28 +34,13 @@ function App() {
     })
   }, [])
 
-  return <FlashcardList flashcards={flashcards} />
+  return (
+    <>
+      <div className="container">
+        <FlashcardList flashcards={flashcards} />
+      </div>
+    </>
+  )
 }
-
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: 'What is 2+2?',
-    answer: '4',
-    options: ['2', '3', '4', '5'],
-  },
-  {
-    id: 2,
-    question: 'Question 2?',
-    answer: 'Answer',
-    options: ['Answer2', 'Answer3', 'Answer4', 'Answer5'],
-  },
-  {
-    id: 3,
-    question: 'Question 3',
-    answer: '4',
-    options: ['Answer2', 'Answer3', 'Answer4', 'Answer5'],
-  },
-]
 
 export default App
--- flashcard-app/src/Flashcard.js
+++ flashcard-app/src/Flashcard.js
@@ -1,13 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 export const Flashcard = ({ id, question, answer, options }) => {
   const [flip, setFlip] = useState(false)
+  const [height, setHeight] = useState('initial')
+
+  const frontEl = useRef()
+  const backEl = useRef()
+
+  const setMaxHeight = () => {
+    const frontHeight = frontEl.current.getBoundingClientRect().height
+    const backHeight = backEl.current.getBoundingClientRect().height
+    setHeight(Math.max(frontHeight, backHeight, 100))
+  }
+
+  // using this useEffect in order to change the card's height according to the height its content takes
+  useEffect(setMaxHeight, [question, answer, options])
+
+  //using this useEffect to adjust the card's height again, this time according to the changes in page size
+  useEffect(() => {
+    window.addEventListener('resize', setMaxHeight)
+
+    //this return will be called whenever our componnent destroys itself
+    return () => window.removeEventListener('resize', setMaxHeight)
+  }, [])
+
   return (
     <div
       className={`card ${flip ? 'flip' : ''}`}
+      style={{ height: height }}
       onClick={() => setFlip(!flip)}
     >
-      <div className="front">
+      <div className="front" ref={frontEl}>
         <p>{question}</p>
         <div className="flashcard-option">
           {options.map((option, i) => {
@@ -20,7 +43,9 @@ export const Flashcard = ({ id, question, answer, options }) => {
         </div>
       </div>
 
-      <div className="back">{answer}</div>
+      <div className="back" ref={backEl}>
+        {answer}
+      </div>
     </div>
   )
 }
