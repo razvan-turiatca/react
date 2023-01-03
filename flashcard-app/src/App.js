ff2c920541f76a14855c8dd28f79f63961923473
razvan-turiatca <codingwithrazvan@gmail.com>
2023-01-03 19:06:04 +0200

    create function for decoding the HTML encoded elements from API

--- flashcard-app/src/App.js
+++ flashcard-app/src/App.js
@@ -6,15 +6,25 @@ import axios from 'axios'
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
 
+  // this function will decode the html encoded elements from the API
+  const decodeString = (str) => {
+    const textArea = document.createElement('textArea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
   useEffect(() => {
     axios.get('https://opentdb.com/api.php?amount=10').then((res) => {
       setFlashcards(
         res.data.results.map((item, index) => {
           const answer = item.correct_answer
-          const options = [...item.incorrect_answers, answer]
+          const options = [
+            ...item.incorrect_answers.map((a) => decodeString(a)),
+
+            decodeString(answer),
+          ]
           return {
             id: `${index}-${Date.now()}`,
-            question: item.question,
+            question: decodeString(item.question),
             answer: answer,
             options: options.sort(() => Math.random() - 0.5),
           }
