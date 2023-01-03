110df2e028eb57609379ca7d5670cd7275b76b14
razvan-turiatca <codingwithrazvan@gmail.com>
2023-01-03 16:58:24 +0200

    add basic design to test the functionality

--- flashcard-app/src/Flashcard.js
+++ flashcard-app/src/Flashcard.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Flashcard = ({ id, question, answer, options }) => {
+  const [flip, setFlip] = useState(false)
   return (
-    <div key={id}>
-      <p>{question}</p>
-      <ul>
-        {options.map((answer, i) => {
-          return <li key={i}>{answer}</li>
-        })}
-      </ul>
+    <div
+      className={`card ${flip ? 'flip' : ''}`}
+      onClick={() => setFlip(!flip)}
+    >
+      <div className="front">
+        <h3>question</h3>
+        <div className="flashcard-option">
+          {options.map((option, i) => {
+            return (
+              <div className="flashcard-option" key={i}>
+                {option}
+              </div>
+            )
+          })}
+        </div>
+      </div>
+
+      <div className="back">{answer}</div>
     </div>
   )
 }
